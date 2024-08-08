5c50ec5f9eb7e6fb9ce2bee283d65dae6e52c31a
TusharGramni <113756375+TusharGramni@users.noreply.github.com>
2024-08-08 09:59:16 +0530

    Some Changes to add more aniamtion

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import Quiz from './Components/Quiz'
+import React from "react";
+import Quiz from "./Components/Quiz";
 import top from "./assets/images/top.svg";
 import bottom from "./assets/images/bottom.svg";
 const App = () => {
   return (
     <div className="Container">
       <div className="wave-container">
-        <img src={top} alt="" className="animated-svg" />
+        <div className="anim-1">
+          <img src={top} alt="" className="animated-svg" />
+          <img src={top} alt="" className="animated-svg" />
+          <img src={top} alt="" className="animated-svg" />
+        </div>
       </div>
       <Quiz />
+
       <div className="wave-container">
-        <img src={bottom} alt="" className="animated-svg" />
+        <div className="anim-2">
+          <img src={bottom} alt="" className="animated-svg" />
+          <img src={bottom} alt="" className="animated-svg" />
+          <img src={bottom} alt="" className="animated-svg" />
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
